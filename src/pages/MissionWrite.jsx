10ee85c854456364eadbed9784eaaa4164f65cd8
hellomargo <developer.margo@gmail.com>
2022-07-01 00:26:35 +0900

    react-hook-form설치, 목표 작성페이지 form작업중

--- src/pages/MissionWrite.jsx
+++ src/pages/MissionWrite.jsx
@@ -1,3 +1,33 @@
+import { useForm } from 'react-hook-form';
+
 export default function MissionWrite() {
-  return <div>목표 등록 페이지</div>;
+  const {
+    register,
+    handleSubmit,
+    onError,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h2>목표 등록 하기</h2>
+
+      <form onSubmit={handleSubmit(onSubmit, onError)}>
+        <div>
+          <label>목표이름</label>
+          <input type="text" {...register('title')} />
+        </div>
+        <div>
+          <label>포부한마디</label>
+          <textarea type="text" {...register('hopes')} />
+        </div>
+
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
