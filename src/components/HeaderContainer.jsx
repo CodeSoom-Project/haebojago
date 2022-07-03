c81743279441848ee8a9ac3ee09c225c4ade4e5b
hellomargo <developer.margo@gmail.com>
2022-07-03 23:19:06 +0900

    헤더, 목표 등록화면 스타일 작업

--- src/components/HeaderContainer.jsx
+++ src/components/HeaderContainer.jsx
@@ -1,13 +1,33 @@
 import { Link } from 'react-router-dom';
 
+import styled from '@emotion/styled';
+
+import CommonStyledComponents from '../shared/styles/CommonStyledComponents';
+
+const { Area } = CommonStyledComponents;
+
+const Header = styled.header`
+  border-bottom: 1px solid #ddd;
+`;
+const HeaderArea = styled(Area)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 60px;
+`;
+
 export default function HeaderContainer() {
   return (
-    <header>
-      <h1>해보자고!</h1>
+    <Header>
+      <HeaderArea>
+        <h1>
+          <Link to="/">해보자고</Link>
+        </h1>
 
-      <nav>
-        <Link to="/">나의 목표</Link>
-      </nav>
-    </header>
+        <nav>
+          <Link to="/">나의 목표</Link>
+        </nav>
+      </HeaderArea>
+    </Header>
   );
 }
--- src/pages/MissionList.jsx
+++ src/pages/MissionList.jsx
@@ -1,22 +1,73 @@
 import { Link } from 'react-router-dom';
 
+import styled from '@emotion/styled';
+
+import CommonStyledComponents from '../shared/styles/CommonStyledComponents';
+
+const { CommonTitle } = CommonStyledComponents;
+
+const MissionItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+  min-height: 200px;
+  border-radius: 10px;
+  line-height: 1.3;
+  padding: 20px 15px;
+  box-shadow: 0 0 10px 10px rgba(0, 0, 0, 0.05);
+`;
+const MissionTitle = styled.p`
+  margin-bottom: 20px;
+  font-weight: 700;
+  font-size: 18px;
+`;
+const MissionSaying = styled.p`
+  color: #333;
+`;
+const MissionCount = styled.p`
+  margin-top: auto;
+`;
+const MissionNoContents = styled.p`
+  margin: 60px 0;
+  font-size: 14px;
+  color: #888;
+  text-align: center;
+`;
+const ButtonItems = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Button = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 60px;
+  width: 120px;
+  height: 50px;
+  background-color: #ace;
+`;
+
 export default function MissionList() {
   const missionItems = JSON.parse(localStorage.getItem('newData'));
 
   return (
     <div>
-      목표 리스트
-      <br />
+      <CommonTitle>목표 리스트</CommonTitle>
+
       {missionItems ? (
-        <div>
-          <p>목표 : {missionItems.title}</p>
-          <p>포부한마디 : {missionItems.saying}</p>
-          <p>인증 : 인증횟수 /  {missionItems.count}</p>
-        </div>
+        <MissionItem>
+          <MissionTitle>{missionItems.title}</MissionTitle>
+          <MissionSaying>{missionItems.saying}</MissionSaying>
+          <MissionCount>인증횟수 / {missionItems.count}</MissionCount>
+        </MissionItem>
       ) : (
-        '새로운 목표를 등록해주세요.'
+        <MissionNoContents>새로운 목표를 등록해주세요.</MissionNoContents>
       )}
-      <Link to="/writemission">목표 등록하기</Link>
+
+      <ButtonItems>
+        <Button to="/writemission">목표 등록하기</Button>
+      </ButtonItems>
     </div>
   );
 }
